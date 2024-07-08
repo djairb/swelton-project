d615b01f059ae488388b28768bfb634479e17ae7
djairb <39774197+djairb@users.noreply.github.com>
2024-07-08 11:10:28 -0300

    adição do segundo formulario

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const formulario = document.getElementById('f1formulario');
 const f1calcularButton = document.getElementById('f1calcular');
 const f1resultado = document.getElementById('f1resultado');
 
+
+const formulario2 = document.getElementById('f2formulario');
+const f2calcularButton = document.getElementById('f2calcular');
+const f2resultado = document.getElementById('f2resultado');
+
 f1calcularButton.addEventListener('click', f1calcularPontuacao); // Add event listener to button
+
+f2calcularButton.addEventListener('click', f2calcularPontuacao); // Add event listener to button
 
 function f1calcularPontuacao() {
   let pontuacao = 0;
@@ -80,4 +87,67 @@ function f1calcularPontuacao() {
   }
   // Update the result element with the calculated score
   f1resultado.textContent = `Sua pontuação: ${pontuacao}`;
+}
+
+function f2calcularPontuacao() {
+  let pontuacao2 = 0;
+
+  // Get values for each question
+  const f2q1 = document.querySelector('input[name="f2q1"]:checked');
+  const f2q2 = document.querySelector('input[name="f2q2"]:checked');
+  const f2q3 = document.querySelector('input[name="f2q3"]:checked');
+  const f2q4 = document.querySelector('input[name="f2q4"]:checked');
+  const f2q5 = document.querySelector('input[name="f2q5"]:checked');
+  const f2q6 = document.querySelector('input[name="f2q6"]:checked');
+  const f2q7 = document.querySelector('input[name="f2q7"]:checked');
+  const f2q8 = document.querySelector('input[name="f2q8"]:checked');
+  const f2q9 = document.querySelector('input[name="f2q9"]:checked');
+  const f2q10 = document.querySelector('input[name="f2q10"]:checked');
+  const f2q11 = document.querySelector('input[name="f2q11"]:checked');
+  const f2q12 = document.querySelector('input[name="f2q12"]:checked');
+  const f2q13 = document.querySelector('input[name="f2q13"]:checked');
+ 
+
+  // Check if any option is selected and add points accordingly
+  if (f2q1) {
+    pontuacao2 += parseInt(f2q1.value);
+  }
+  if (f2q2) {
+    pontuacao2 += parseInt(f2q2.value);
+  }
+  if (f2q3) {
+    pontuacao2 += parseInt(f2q3.value);
+  }
+  if (f2q4) {
+    pontuacao2 += parseInt(f2q4.value);
+  }
+  if (f2q5) {
+    pontuacao2 += parseInt(f2q5.value);
+  }
+  if (f2q6) {
+    pontuacao2 += parseInt(f2q6.value);
+  }
+  if (f2q7) {
+    pontuacao2 += parseInt(f2q7.value);
+  }
+  if (f2q8) {
+    pontuacao2 += parseInt(f2q8.value);
+  }
+  if (f2q9) {
+    pontuacao2 += parseInt(f2q9.value);
+  }
+  if (f2q10) {
+    pontuacao2 += parseInt(f2q10.value);
+  }
+  if (f2q11) {
+    pontuacao2 += parseInt(f2q11.value);
+  }
+  if (f2q12) {
+    pontuacao2 += parseInt(f2q12.value);
+  }
+  if (f2q13) {
+    pontuacao2 += parseInt(f2q13.value);
+  }
+  // Update the result element with the calculated score
+  f2resultado.textContent = `Sua pontuação: ${pontuacao2}`;
 }
